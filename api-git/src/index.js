4b7bbf6cbd9534cc3468e1ad6212ef59e8aaccc6
Yago Macinelli <yagom.ym@gmail.com>
2023-03-21 22:01:57 -0300

    Remove: => Function buildObjectFork.

--- api-git/src/index.js
+++ api-git/src/index.js
@@ -16,28 +16,28 @@ function searchCommits() {
 
 //Obtain all values from form
 function obtainValuesInput() {
-    this.urlComplete = document.getElementById("urlComplete").value;
+    urlComplete = document.getElementById("urlComplete").value;
 
-    if (this.urlComplete) {
-        const cleanUrl = this.urlComplete.replace("https://github.com/", '');
+    if (urlComplete) {
+        const cleanUrl = urlComplete.replace("https://github.com/", '');
         const indexBarLine = cleanUrl.indexOf('/');
 
-        this.ownerRepo = cleanUrl.substring(0, indexBarLine);
-        this.nameRepo = cleanUrl.substring((indexBarLine + 1), cleanUrl.length);
+        ownerRepo = cleanUrl.substring(0, indexBarLine);
+        nameRepo = cleanUrl.substring((indexBarLine + 1), cleanUrl.length);
     } else {
-        this.ownerRepo = document.getElementById("ownerRepo").value;
-        this.nameRepo = document.getElementById("nameRepo").value;
+        ownerRepo = document.getElementById("ownerRepo").value;
+        nameRepo = document.getElementById("nameRepo").value;
     }
 
-    this.startDate = document.getElementById("startDate").value;
-    this.endDate = document.getElementById("endDate").value;
+    startDate = document.getElementById("startDate").value;
+    endDate = document.getElementById("endDate").value;
 }
 
 //Call Api Git to obtain JSON with information of commits
 function callApiCommits() {
-    let url = `${apiGit}/${this.ownerRepo}/${this.nameRepo}/commits`;
+    let url = `${apiGit}/${ownerRepo}/${nameRepo}/commits`;
 
-    if (this.startDate && this.endDate) url = `${url}?since=${this.startDate}&until=${this.endDate}`;
+    if (startDate && endDate) url = `${url}?since=${startDate}&until=${endDate}`;
 
     fetch(url)
         .then(response => {
@@ -53,45 +53,39 @@ function buildMapDataCommits(json) {
 
     json.forEach(element => {
         const dateCommit = element.commit.author.date.substring(0, 10);
-        const messageCommit = { message: element.commit.message };
+        const messageCommit = {message: element.commit.message};
 
         if (commitsPerDay[dateCommit]) {
             commitsPerDay[dateCommit].message.push(messageCommit);
             commitsPerDay[dateCommit].count++;
         } else {
-            commitsPerDay[dateCommit] = {count: 1, date: dateCommit, message: [ messageCommit ]};
+            commitsPerDay[dateCommit] = {count: 1, date: dateCommit, message: [messageCommit]};
         }
     });
 
-    if (commitsPerDay.size) this.buildTableDataCommits(commitsPerDay);
+    if (commitsPerDay) this.buildTableDataCommits(commitsPerDay);
 }
 
 //Call Api Git to obtain information about forks and starring
 function callApiForks() {
-    let url = `${apiGit}/${this.ownerRepo}/${this.nameRepo}/forks`;
+    let url = `${apiGit}/${ownerRepo}/${nameRepo}/forks`;
 
     fetch(url)
         .then(response => {
             if (response.status === 200) return response.json();
         })
         .then(json => {
-            if (json.size) this.buildMapDataForks(json);
+            if (json) this.buildMapDataForks(json);
         });
 }
 
 function buildMapDataForks(json) {
     let forkMap = {};
     json.forEach(element => {
-       forkMap = { forks_count: element.forks_count, stargazers_count: element.stargazers_count };
+        forkMap = {forks_count: element.forks_count, stargazers_count: element.stargazers_count};
     });
 
-    if (forkMap.size) this.buildTableDataForks(forkMap);
-}
-
-function buildObjectForks(map) {
-    return Object.keys(map).map(() => {
-        return {forks_count: map.forks_count, stargazers_count: map.stargazers_count}
-    });
+    if (forkMap) this.buildTableDataForks(forkMap);
 }
 
 function buildObjectCommits(map) {
@@ -112,15 +106,14 @@ function buildTableDataForks(map) {
     thead.appendChild(th_star).innerText = 'Stars Count'
     table.appendChild(tbody);
 
-    this.buildObjectForks(map).forEach(element => {
-        let trow = tbody.insertRow();
 
-        const tdata_fork = trow.insertCell();
-        tdata_fork.innerText = element.forks_count;
+    let trow = tbody.insertRow();
 
-        const tdata_star = trow.insertCell();
-        tdata_star.innerText = element.stargazers_count;
-    });
+    const tdata_fork = trow.insertCell();
+    tdata_fork.innerText = map.forks_count;
+
+    const tdata_star = trow.insertCell();
+    tdata_star.innerText = map.stargazers_count;
 
     //Clean content before append
     document.getElementById("table-forks").innerHTML = '';
