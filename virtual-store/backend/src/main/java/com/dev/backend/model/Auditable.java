103e0d8923da959d1b17103790caad9ce1367bbd
yamacinelli <yagom.ym@gmail.com>
2023-06-13 22:19:28 -0300

    hotfix:  - annotation @Temporal in Auditable.

--- virtual-store/backend/src/main/java/com/dev/backend/model/Auditable.java
+++ virtual-store/backend/src/main/java/com/dev/backend/model/Auditable.java
@@ -3,13 +3,17 @@ package com.dev.backend.model;
 import lombok.Data;
 
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.util.Date;
 
 @Data
 @MappedSuperclass
 public class Auditable {
 
+    @Temporal(TemporalType.TIMESTAMP)
     private Date createdDate;
 
+    @Temporal(TemporalType.TIMESTAMP)
     private Date editedDate;
 }
