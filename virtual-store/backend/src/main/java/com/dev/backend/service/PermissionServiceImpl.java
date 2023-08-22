9b831d3ee8ef8ede7b3b4768162ce2a6c3035079
yamacinelli <yagom.ym@gmail.com>
2023-08-22 15:45:38 -0300

    fix:  - add layer of interface controller in Permission files.

---
+++ virtual-store/backend/src/main/java/com/dev/backend/service/PermissionServiceImpl.java
@@ -0,0 +1,55 @@
+package com.dev.backend.service;
+
+import com.dev.backend.dto.PermissionDto;
+import com.dev.backend.model.Permission;
+import com.dev.backend.repository.PermissionRepository;
+import com.dev.backend.utils.ParseUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PermissionServiceImpl implements PermissionService {
+
+    private final PermissionRepository permissionRepository;
+
+    public PermissionServiceImpl(PermissionRepository permissionRepository) {
+        this.permissionRepository = permissionRepository;
+    }
+
+    @Override
+    public PermissionDto save(PermissionDto dto) {
+        return ParseUtils.parse(
+                permissionRepository.saveAndFlush(ParseUtils.parse(dto, Permission.class)),
+                PermissionDto.class);
+    }
+
+    @Override
+    public List<PermissionDto> saveAll(List<PermissionDto> dtos) {
+        return ParseUtils.parse(
+                permissionRepository.saveAllAndFlush(ParseUtils.parse(dtos, Permission.class)),
+                PermissionDto.class);
+    }
+
+    @Override
+    public PermissionDto findById(Integer id) {
+        return ParseUtils.parse(permissionRepository.findById(id).orElse(null), PermissionDto.class);
+    }
+
+    @Override
+    public List<PermissionDto> findAll() {
+        return ParseUtils.parse(permissionRepository.findAll(), PermissionDto.class);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        permissionRepository.deleteById(id);
+    }
+
+    @Override
+    public PermissionDto update(PermissionDto dto) {
+        return ParseUtils.parse(
+                permissionRepository.saveAndFlush(ParseUtils.parse(dto, Permission.class)),
+                PermissionDto.class);
+    }
+}
