c8f8f5ea776c7529179337a137b8fb8bfcfbb87f
yamacinelli <yagom.ym@gmail.com>
2023-08-22 15:42:43 -0300

    fix:  - add layer of interface controller in Image files.

--- virtual-store/backend/src/main/java/com/dev/backend/service/ImageService.java
+++ virtual-store/backend/src/main/java/com/dev/backend/service/ImageService.java
@@ -1,56 +1,7 @@
 package com.dev.backend.service;
 
 import com.dev.backend.dto.ImageDto;
-import com.dev.backend.model.Image;
 import com.dev.backend.model.GenericModel;
-import com.dev.backend.repository.ImageRepository;
-import com.dev.backend.utils.ParseUtils;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-@Service
-public class ImageService implements GenericModel<ImageDto> {
-
-    private final ImageRepository imageRepository;
-
-    public ImageService(ImageRepository imageRepository) {
-        this.imageRepository = imageRepository;
-    }
-
-    @Override
-    public ImageDto save(ImageDto dto) {
-        return ParseUtils.parse(
-                imageRepository.saveAndFlush(ParseUtils.parse(dto, Image.class)),
-                ImageDto.class);
-    }
-
-    @Override
-    public List<ImageDto> saveAll(List<ImageDto> dtos) {
-        return ParseUtils.parse(
-                imageRepository.saveAllAndFlush(ParseUtils.parse(dtos, Image.class)),
-                ImageDto.class);
-    }
-
-    @Override
-    public ImageDto findById(Integer id) {
-        return ParseUtils.parse(imageRepository.findById(id).orElse(null), ImageDto.class);
-    }
-
-    @Override
-    public List<ImageDto> findAll() {
-        return ParseUtils.parse(imageRepository.findAll(), ImageDto.class);
-    }
-
-    @Override
-    public void delete(Integer id) {
-        imageRepository.deleteById(id);
-    }
-
-    @Override
-    public ImageDto update(ImageDto dto) {
-        return ParseUtils.parse(
-                imageRepository.saveAndFlush(ParseUtils.parse(dto, Image.class)),
-                ImageDto.class);
-    }
+public interface ImageService extends GenericModel<ImageDto> {
 }
---
+++ virtual-store/backend/src/main/java/com/dev/backend/service/ImageServiceImpl.java
@@ -0,0 +1,55 @@
+package com.dev.backend.service;
+
+import com.dev.backend.dto.ImageDto;
+import com.dev.backend.model.Image;
+import com.dev.backend.repository.ImageRepository;
+import com.dev.backend.utils.ParseUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ImageServiceImpl implements ImageService {
+
+    private final ImageRepository imageRepository;
+
+    public ImageServiceImpl(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+
+    @Override
+    public ImageDto save(ImageDto dto) {
+        return ParseUtils.parse(
+                imageRepository.saveAndFlush(ParseUtils.parse(dto, Image.class)),
+                ImageDto.class);
+    }
+
+    @Override
+    public List<ImageDto> saveAll(List<ImageDto> dtos) {
+        return ParseUtils.parse(
+                imageRepository.saveAllAndFlush(ParseUtils.parse(dtos, Image.class)),
+                ImageDto.class);
+    }
+
+    @Override
+    public ImageDto findById(Integer id) {
+        return ParseUtils.parse(imageRepository.findById(id).orElse(null), ImageDto.class);
+    }
+
+    @Override
+    public List<ImageDto> findAll() {
+        return ParseUtils.parse(imageRepository.findAll(), ImageDto.class);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        imageRepository.deleteById(id);
+    }
+
+    @Override
+    public ImageDto update(ImageDto dto) {
+        return ParseUtils.parse(
+                imageRepository.saveAndFlush(ParseUtils.parse(dto, Image.class)),
+                ImageDto.class);
+    }
+}
