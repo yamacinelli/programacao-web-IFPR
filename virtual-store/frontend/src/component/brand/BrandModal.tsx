e87608c9ebfa1f713d42835e847a8c2b8fb78fe7
yamacinelli <yagom.ym@gmail.com>
2023-10-08 20:43:54 -0300

    feature: <message on create brand>

--- virtual-store/frontend/src/component/brand/BrandModal.tsx
+++ virtual-store/frontend/src/component/brand/BrandModal.tsx
@@ -6,24 +6,36 @@ import {
     Modal, ModalBody, ModalCloseButton,
     ModalContent, ModalFooter,
     ModalHeader,
-    ModalOverlay
+    ModalOverlay, useToast
 } from "@chakra-ui/react";
 import {Brand} from "../../model/Brand";
 import BrandService from "../../service/BrandService";
-import {ChangeEventHandler} from "react";
+import {ChangeEventHandler, useState} from "react";
 
 const BrandModal = ({ isOpen, onOpen, onClose, initialRef }: any) => {
 
+    const [ brand, setBrand ] = useState(new Brand());
     const service = new BrandService;
+    const toast = useToast();
 
     const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-        console.log(e);
+        const { name, value } = e.target;
+        setBrand(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
     }
 
     const handleSave = (brand: Brand) => {
-        service.save(brand).then(response => {
-
-        });
+        service.save(brand)
+            .then(response => {
+                toast({ description: 'Success on create brand', status: "success" });
+                onClose();
+            })
+            .catch(error => {
+                toast({ description: 'Error on create brand', status: "error" });
+                console.error(error);
+            });
     }
 
     return (
@@ -39,11 +51,11 @@ const BrandModal = ({ isOpen, onOpen, onClose, initialRef }: any) => {
                 <ModalBody pb={6}>
                     <FormControl variant={'floating'}>
                         <FormLabel>Name</FormLabel>
-                        <Input ref={initialRef} placeholder={''} onChange={handleChange} />
+                        <Input name={'name'} ref={initialRef} placeholder={''} onChange={handleChange} />
                     </FormControl>
                 </ModalBody>
                 <ModalFooter>
-                    <Button colorScheme="blue" mr={3}>Save</Button>
+                    <Button colorScheme="blue" mr={3} onClick={() => handleSave(brand)}>Save</Button>
                     <Button onClick={onClose}>Cancel</Button>
                 </ModalFooter>
             </ModalContent>
